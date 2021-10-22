e9eaf8e422fc513c475bc65be223355664d2cb59
Brinton <brintonfoyreed@gmail.com>
2021-10-22 11:44:59 -0700

    Change variable from state, questions container

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,74 +1,87 @@
-import React, { Component } from 'react'
-import ResultsChart from './ResultsChart'
-import ResultsTable from './ResultsTable'
-import Footer from './Footer'
-import '../index.css';
-import Test from './Test';
-
+import React, { Component } from "react";
+import ResultsChart from "./ResultsChart";
+import ResultsTable from "./ResultsTable";
+import Footer from "./Footer";
+import "../index.css";
 
 class Results extends Component {
-    
-    state = {
-        countedResults: {},
-        finishedCounting: false
+  state = {
+    countedResults: {},
+    finishedCounting: false,
+  };
+
+  //? COUNTS NUMBER OF RESULTS AND CREATES OBJECT
+  resultsCount = () => {
+    let initialResults = Array.from(this.props.initialResults);
+    const resultsObj = {};
+    if (!!initialResults) {
+      initialResults.map((result) => {
+        Object.keys(resultsObj).includes(result)
+          ? (resultsObj[result] += 1)
+          : (resultsObj[result] = 1);
+      });
+      this.setState({
+        countedResults: resultsObj,
+        finishedCounting: true,
+      });
     }
+  };
 
-    resultsCount = () => {      //? COUNTS FREQUENCY OF RESULTS AND CREATES OBJECT
-        let initialResults = Array.from(this.props.initialResults)
-        const resultsObj = {}
-        if(!!initialResults) {
-            initialResults.map(result => {
-                Object.keys(resultsObj).includes(result) ? resultsObj[result] += 1 : resultsObj[result] = 1
-            })
-            this.setState({
-                countedResults: resultsObj,
-                finishedCounting: true
-            })
-        }
-        }
+  //? POSTS NAME AND SCORE TO JSON SERVER
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let name = e.target.firstChild.value;
+    let request = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: "",
+        name: name,
+        results: this.state.countedResults,
+      }),
+    };
+    fetch("http://localhost:3001/results", request)
+      .then((response) => response.json())
+      .then((data) => console.log("POST", data));
+  };
 
-    handleSubmit = (e) => {         //? POSTS NAME AND SCORE TO JSON SERVER
-        e.preventDefault() 
-        let name = e.target.firstChild.value
-        let request = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 
-                    "id": "",
-                    "name": name,
-                    "results": this.state.countedResults
-                 })
-            };
-        fetch('http://localhost:3001/results', request)
-            .then(response => response.json())
-            .then(data => console.log("POST", data))
-            }
+  //? DISPLAYS RESULTS AND HOLDS POST FORM
+  render() {
+    return (
+      <div id='results'>
+        <div className='row'>
+          <div className='column'>
+            <h1>Your Results</h1>
+          </div>
+          <div className='column'>
+            <form className='form' onSubmit={this.handleSubmit}>
+              <input type='text' placeholder='Your Name' style={{ fontSize: 20 }}></input>
+              <button className='button' style={{ fontSize: 18 }}>
+                Save Your Score
+              </button>
+            </form>
+          </div>
+        </div>
+        <div style={{ backgroundColor: "white" }}>
+          {this.state.finishedCounting === false ? (
+            this.resultsCount()
+          ) : (
+            <ResultsChart className='chart' countedResults={this.state.countedResults} />
+          )}
+        </div>
+        <ResultsTable />
+        <p>
+          For more information on the Enneagram, your Enneatype, or to take the full 144-question
+          Riso-Hudson Enneagram Type Indicator, click{" "}
+          <a className='blackLink' href='https://www.enneagraminstitute.com/'>
+            here.
+          </a>
+        </p>
+        <hr></hr>
+        <Footer />
+      </div>
+    );
+  }
+}
 
-render() {              //? DISPLAYS RESULTS AND HOLDS POST FORM
-        return ( 
-            <div id="results">
-                <div className="row">
-                    <div className="column"> 
-                        <h1>Your Results</h1> 
-                    </div>
-                    <div className="column">
-                        <form className="form" onSubmit={this.handleSubmit}>
-                            <input type="text" placeholder="Your Name" style={{fontSize:20}}></input>
-                            <button className="button" style={{fontSize:18}}>Save Your Score</button>
-                        </form>
-                    </div>
-                    
-                </div>
-                <div style={{backgroundColor: 'white'}}>
-                    {(this.state.finishedCounting === false) ? this.resultsCount() : <ResultsChart className="chart" countedResults={this.state.countedResults} />}
-                </div>
-                    <ResultsTable />
-                    <p>For more information on the Enneagram, your Enneatype, or to take the full 144-question Riso-Hudson Enneagram Type Indicator, click <a className="blackLink" href="https://www.enneagraminstitute.com/">here.</a></p>
-                    <hr></hr>
-                    <Footer />
-            </div>
-         );
-        }
-    }
-
-    export default Results;
+export default Results;
--- src/Containers/QuestionsContainer.js
+++ src/Containers/QuestionsContainer.js
@@ -1,64 +1,73 @@
-import React, { Component } from 'react';
-import Questions from '../Components/Questions';
-import '../index.css';
+import React, { Component } from "react";
+import Questions from "../Components/Questions";
+import "../index.css";
 
 class QuestionsContainer extends Component {
-    state = { 
-        questions: [],
-        startingIndex: 0,
-        initialResults: [],
-        finished: false 
-     }
+  state = {
+    questions: [],
+    startingIndex: 0,
+    initialResults: [],
+    finished: false,
+  };
 
-    componentDidMount(){
-         this.fetchQuestions()
-     }
+  componentDidMount() {
+    this.fetchQuestions();
+  }
 
-    fetchQuestions = () => {            //? GETS QUESTION ARRAY
-        fetch("http://localhost:3001/questions")
-        .then(res => res.json())
-        .then(questionsArr => {
-            this.setState({
-                questions: questionsArr,
-            })
-        })
-     }
+  fetchQuestions = () => {
+    //? GETS QUESTION ARRAY
+    fetch("http://localhost:3001/questions")
+      .then((res) => res.json())
+      .then((questionsArr) => {
+        this.setState({
+          questions: questionsArr,
+        });
+      });
+  };
 
-    renderTwoQuestions = () => {            //? SLICES FIRST SET OF TWO QUESTIONS, KILLS PROCESS WHEN FINISHED
-        const {questions, startingIndex} = this.state   
-        const twoQuestions = questions.slice(startingIndex, startingIndex + 2)
-        return (startingIndex >= 72) ? this.processResults() : twoQuestions
-        // 72 total questions
-    }
-    
-    nextQuestions = (e) => {            //? COUNTER FOR QUESTION SETS
-        this.setState(prevState => {
-            return{
-                startingIndex: prevState.startingIndex + 2,
-                initialResults: [...this.state.initialResults, e.target.value]
-            }
-        })
-        this.renderTwoQuestions()
-    }
-    
-    processResults = () => {            //? NAVIGATES TO RESULTS COMPONENT WHEN FINISHED
-        this.state.finished = true
-        this.props.history.push('/results')
-        return []
-    }
-    
-    renderResults = () => {this.props.resultsToState(this.state.initialResults)}            //? SENDS RESULTS TO APP.JS WHEN FINISHED
+  //? SLICES FIRST SET OF TWO QUESTIONS, KILLS PROCESS WHEN FINISHED
+  renderTwoQuestions = () => {
+    const { questions, startingIndex } = this.state;
+    const twoQuestions = questions.slice(startingIndex, startingIndex + 2);
+    return startingIndex >= 72 ? this.processResults() : twoQuestions;
+    // 72 total questions
+  };
 
-    render() { 
-        let renderQuestions = this.renderTwoQuestions().map(question => <Questions question={question} id={question.id} nextQuestions={this.nextQuestions} />)
-        return(     
-            <div className="App">
-                <br></br>
-                <h1>Select One</h1>
-                {(this.state.finished) ? this.renderResults() : renderQuestions }
-            </div>
-        )
-    }
+  //? COUNTER FOR QUESTION SETS
+  nextQuestions = (e) => {
+    this.setState((prevState) => {
+      return {
+        startingIndex: prevState.startingIndex + 2,
+        initialResults: [...this.state.initialResults, e.target.value],
+      };
+    });
+    this.renderTwoQuestions();
+  };
+
+  //? NAVIGATES TO RESULTS COMPONENT WHEN FINISHED
+  processResults = () => {
+    this.state.finished = true;
+    this.props.history.push("/results");
+    return [];
+  };
+
+  //? SENDS RESULTS TO APP.JS WHEN FINISHED
+  renderResults = () => {
+    this.props.resultsToState(this.state.initialResults);
+  };
+
+  render() {
+    let renderQuestions = this.renderTwoQuestions().map((question) => (
+      <Questions question={question} id={question.id} nextQuestions={this.nextQuestions} />
+    ));
+    return (
+      <div className='App'>
+        <br></br>
+        <h1>Select One</h1>
+        {this.state.finished ? this.renderResults() : renderQuestions}
+      </div>
+    );
+  }
 }
-    
+
 export default QuestionsContainer;
